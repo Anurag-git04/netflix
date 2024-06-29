2e0b57c88fac85905e93809219ba151369595e06
Anurag-git04 <shawanurag18@gmail.com>
2024-06-29 18:16:51 +0530

    new

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png' alt='netflix-logo'/>
+    </div>
+  )
+}
+
+export default Header
