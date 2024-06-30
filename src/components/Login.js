be58c55c19c96ce41d8c4e977362300df6cbe3ad
Anurag-git04 <shawanurag18@gmail.com>
2024-06-30 20:56:06 +0530

    mm

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,8 @@ const Login = () => {
   return (
     <div>
         <Header/>
-        <div>
-            Login
+        <div className='w-full'>
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/14a8fe85-b6f4-4c06-8eaf-eccf3276d557/IN-en-20230911-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt='banner'/>
         </div>
     </div>
   )
