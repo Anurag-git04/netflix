e957af8569a2e6b34d2dce4d55a52b30cf386372
Anurag-git04 <shawanurag18@gmail.com>
2024-07-10 23:56:57 +0530

    10 july

--- src/components/VBg.js
+++ src/components/VBg.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const VBg = () => {
   return (
-    <div>
-        VedioBackground
+    <div className='w-screen'>
+        <iframe 
+          className='w-screen aspect-video' 
+          src="https://www.youtube.com/embed/pjhHfn-d63I?si=tmBvBjj9CVcpqfTa&autoplay=1&mute=1" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allowfullscreen>
+        </iframe>
     </div>
   )
 }
--- src/components/Vtile.js
+++ src/components/Vtile.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { CiPlay1 } from "react-icons/ci";
+import { CiCircleInfo } from "react-icons/ci";
 
 const Vtile = () => {
   return (
-    <div>Vtitle</div>
+    <div className='absolute text-white pt-[18%] p-12'>
+      <h1 className='text-3xl font-bold'>Anurag Sahw </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure necessitatibus ipsum. Tempore nobis corrupti ex </p>
+      <div className='flex mt-8'>
+        <button className='flex items-center px-6 py-2 bg-white text-black rounded-md hover:bg-opacity-80'>
+          <CiPlay1 />
+          <span>Play</span>
+        </button>
+        <button className='flex mx-2 items-center px-6 py-2 bg-gray-500 opacity-50 text-black rounded-md hover:bg-opacity-80'>
+          <CiCircleInfo />
+          <span>Watch more</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
